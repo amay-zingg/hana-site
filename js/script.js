a2480b4d6210be79c1106b73d12962fd833b7248
amay-zingg <anastasiamay29@gmail.com>
2021-01-15 04:25:27 -0500

    Edited the store page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // * * * * SMOOTH SCROLLING ON SAME PAGE LINKS
-$("a[href^=\\#]").click(function(e) {   
+const scroll = function () {
+    $("a[href^=\\#]").click(function(e) {   
     e.preventDefault();   
     var dest = $(this).attr('href');   
     console.log(dest);   
@@ -8,12 +9,13 @@ $("a[href^=\\#]").click(function(e) {
         , 'slow'
     ); 
 });
+};
 
 // * * * * TICKER TAPE MARQUEE
 const banner = function () {
     let $banner = $('.announcement-banner').marquee();
     let $banner2 = $('.responsive-announcement-banner').marquee();
-}
+};
 
 // * * * * RESPONSIVE MENU
 const navMenu = function () {
@@ -29,11 +31,53 @@ const navMenu = function () {
     });
 };
 
+// SELECTED PHOTO FROM THUMBNAIL OPENS IN FEATURED PHOTO AREA
+const productPhotos = function () {
+    const featuredPhoto = document.querySelector('.featured-photo');
+    const secondPhoto = document.querySelector('.second-photo');
+    const thirdPhoto = document.querySelector('.third-photo');
+
+    function onPageLoad() {
+        secondPhoto.classList.add('unselected');
+        thirdPhoto.classList.add('unselected');
+    };
+
+    onPageLoad();
+
+    document.querySelector('.select-featured-photo').addEventListener('click', function() {
+        featuredPhoto.classList.add('selected');
+        featuredPhoto.classList.remove('unselected');
+        secondPhoto.classList.add('unselected');
+        secondPhoto.classList.remove('selected');
+        thirdPhoto.classList.add('unselected');
+        thirdPhoto.classList.remove('selected');
+    });
+
+    document.querySelector('.select-second-photo').addEventListener('click', function() {
+        featuredPhoto.classList.add('unselected');
+        featuredPhoto.classList.remove('selected');
+        secondPhoto.classList.add('selected');
+        secondPhoto.classList.remove('unselected');
+        thirdPhoto.classList.add('unselected');
+        thirdPhoto.classList.remove('selected');
+    });
+
+    document.querySelector('.select-third-photo').addEventListener('click', function() {
+        featuredPhoto.classList.add('unselected');
+        featuredPhoto.classList.remove('selected');
+        secondPhoto.classList.add('unselected');
+        secondPhoto.classList.remove('selected');
+        thirdPhoto.classList.add('selected');
+        thirdPhoto.classList.remove('unselected');
+    });
+};
+
 // * * * * INIT PIECES
 init = function () {
-    // navigationScroll();
+    scroll();
     banner();
     navMenu();
+    productPhotos();
 };
 
 // * * * * DOCUMENT READY
