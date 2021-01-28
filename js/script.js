6709c46c1c7f263ee17c806b52bfe4eaf615e7e6
amay-zingg <anastasiamay29@gmail.com>
2021-01-27 23:42:24 -0500

    fixed reviews section

--- js/script.js
+++ js/script.js
@@ -11,12 +11,6 @@ const scroll = function () {
 });
 };
 
-// * * * * TICKER TAPE MARQUEE
-const banner = function () {
-    let $banner = $('.announcement-banner').marquee();
-    let $banner2 = $('.responsive-announcement-banner').marquee();
-};
-
 // * * * * RESPONSIVE MENU
 const navMenu = function () {
     // * * * * MENU CLICK OUTSIDE
@@ -31,71 +25,10 @@ const navMenu = function () {
     });
 };
 
-// https://github.com/kenwheeler/slick/
-// https://kenwheeler.github.io/slick/
-// * * * * CAROUSEL OF PHOTOS
-const carousel = function() {
-    $(".vertical-center").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-      });
-      console.log("hello");
-
-    //   $(".lazy").slick({
-    //     lazyLoad: 'ondemand', // ondemand progressive anticipated
-    //     infinite: true
-    //   });
-}
-
-// SELECTED PHOTO FROM THUMBNAIL OPENS IN FEATURED PHOTO AREA
-const productPhotos = function () {
-    const featuredPhoto = document.querySelector('.featured-photo');
-    const secondPhoto = document.querySelector('.second-photo');
-    const thirdPhoto = document.querySelector('.third-photo');
-
-    function onPageLoad() {
-        secondPhoto.classList.add('unselected');
-        thirdPhoto.classList.add('unselected');
-    };
-
-    onPageLoad();
-
-    document.querySelector('.select-featured-photo').addEventListener('click', function() {
-        featuredPhoto.classList.add('selected');
-        featuredPhoto.classList.remove('unselected');
-        secondPhoto.classList.add('unselected');
-        secondPhoto.classList.remove('selected');
-        thirdPhoto.classList.add('unselected');
-        thirdPhoto.classList.remove('selected');
-    });
-
-    document.querySelector('.select-second-photo').addEventListener('click', function() {
-        featuredPhoto.classList.add('unselected');
-        featuredPhoto.classList.remove('selected');
-        secondPhoto.classList.add('selected');
-        secondPhoto.classList.remove('unselected');
-        thirdPhoto.classList.add('unselected');
-        thirdPhoto.classList.remove('selected');
-    });
-
-    document.querySelector('.select-third-photo').addEventListener('click', function() {
-        featuredPhoto.classList.add('unselected');
-        featuredPhoto.classList.remove('selected');
-        secondPhoto.classList.add('unselected');
-        secondPhoto.classList.remove('selected');
-        thirdPhoto.classList.add('selected');
-        thirdPhoto.classList.remove('unselected');
-    });
-};
-
 // * * * * INIT PIECES
 init = function () {
     scroll();
-    banner();
     navMenu();
-    carousel();
-    productPhotos();
 };
 
 // * * * * DOCUMENT READY
