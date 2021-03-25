4510cc8953e2989d09cfe62abc304f3a952e3a13
amay-zingg <anastasiamay29@gmail.com>
2021-03-25 04:28:36 -0400

    Finished updated JS populated info change

--- js/script.js
+++ js/script.js
@@ -418,6 +418,191 @@ const eventArray = [
         title: "Hana Shafi Book Fall 2020",
         image: "./assets/hanaPhotos/HanaShafi-BookFall2020.jpg",
         imageAlt: "Hana Shafi Book Fall 2020 | Frizz Kid Art"
+    },
+    {
+        title: "Small, Broke, and Kind of Dirty Book Baby",
+        image: "./assets/hanaPhotos//HanaShafi-BookBaby.jpg",
+        imageAlt: "Small, Broke, and Kind of Dirty Book Baby | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi Small, Broke, and Kind of Dirty Farm Book Reading 2020",
+        image: "./assets/hanaPhotos/HanaShafi-FarmBookReading2020.jpg",
+        imageAlt: "Hana Shafi Book Fall 2020 | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi & Group - Battle Of The Bards",
+        image: "./assets/events/BattleOfTheBards.jpg",
+        imageAlt: "Hana Shafi & Group - Battle Of The Bards | Frizz Kid Art"
+    },
+    {
+        title: "Mural for CWF 2019",
+        image: "./assets/events/MuralforCWF2019.jpg",
+        imageAlt: "Mural for CWF 2019 | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi - Basement Revue Reading",
+        image: "./assets/events/BasementRevueReading.jpg",
+        imageAlt: "Hana Shafi - Basement Revue Reading | Frizz Kid Art"
+    },
+    {
+        title: "Toronto Biennial Performance 2019",
+        image: "./assets/events/TorontoBiennialPerformance2019-2.jpg",
+        imageAlt: "Toronto Biennial Performance 2019 | Frizz Kid Art"
+    },
+    {
+        title: "Toronto Biennial Performance 2019",
+        image: "./assets/events/TorontoBiennialPerformance2019.jpg",
+        imageAlt: "Toronto Biennial Performance 2019 | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art Window Display for TKVO",
+        image: "./assets/events/WindowDisplayForTKVO-3.jpg",
+        imageAlt: "Window Display for TKVO | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art Window Display for TKVO",
+        image: "./assets/events/WindowDisplayForTKVO-2.jpg",
+        imageAlt: "Window Display for TKVO | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art Window Display for TKVO",
+        image: "./assets/events/WindowDisplayForTKVO.jpg",
+        imageAlt: "Window Display for TKVO | Frizz Kid Art"
+    },
+    {
+        title: "Window Display for Hard Feelings",
+        image: "./assets/events/WindowDisplayForHardFeelings-2.jpg",
+        imageAlt: "Window Display for Hard Feelings | Frizz Kid Art"
+    },
+    {
+        title: "Window Display for Hard Feelings",
+        image: "./assets/events/WindowDisplayForHardFeelings-1.jpg",
+        imageAlt: "Window Display for Hard Feelings | Frizz Kid Art"
+    },
+    {
+        title: "Window Display for Hard Feelings",
+        image: "./assets/events/WindowDisplayForHardFeelings.jpg",
+        imageAlt: "Window Display for Hard Feelings | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi Reading",
+        image: "./assets/events/Reading.jpg",
+        imageAlt: "Hana Shafi Reading | Frizz Kid Art"
+    },
+    {
+        title: "University Of Oregon Speaking",
+        image: "./assets/events/UniversityOfOregonSpeaking-3.jpg",
+        imageAlt: "University Of Oregon Speaking | Frizz Kid Art"
+    },
+    {
+        title: "University Of Oregon Speaking",
+        image: "./assets/events/UniversityOfOregonSpeaking-2.jpg",
+        imageAlt: "University Of Oregon Speaking | Frizz Kid Art"
+    },
+    {
+        title: "University Of Oregon Speaking",
+        image: "./assets/events/UniversityOfOregonSpeaking.jpg",
+        imageAlt: "University Of Oregon Speaking | Frizz Kid Art"
+    },
+    {
+        title: "Calgary Word Fest 2018",
+        image: "./assets/events/CalgaryWordFest2018.jpg",
+        imageAlt: "Calgary Word Fest 2018 | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi Reading",
+        image: "./assets/hanaPhotos/HanaShafiReading.jpg",
+        imageAlt: "Hana Shafi Reading | Frizz Kid Art"
+    },
+    {
+        title: "Reading At The Snail",
+        image: "./assets/events/ReadingAtTheSnail.jpg",
+        imageAlt: "Reading At The Snail | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi It Begins with the Body Book Launch",
+        image: "./assets/events/GladDayFirstBookLaunch-2.jpg",
+        imageAlt: "Hana Shafi It Begins with the Body Book Launch | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi It Begins with the Body Book Launch",
+        image: "./assets/events/GladDayFirstBookLaunch-3.jpg",
+        imageAlt: "Hana Shafi It Begins with the Body Book Launch | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi It Begins with the Body Book Launch",
+        image: "./assets/events/GladDayFirstBookLaunch.jpg",
+        imageAlt: "Hana Shafi It Begins with the Body Book Launch | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi First Book for sale in Indigo",
+        image: "./assets/events/FirstBookIndigo.jpg",
+        imageAlt: "Hana Shafi First Book for sale in Indigo | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi with It Begins with the Body",
+        image: "./assets/events/FirstBook.jpg",
+        imageAlt: "Hana Shafi with It Begins with the Body | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi in the Do You See What I See? Art Show",
+        image: "./assets/events/DoYouSeeWhatISee2018-2.jpg",
+        imageAlt: "Hana Shafi in the Do You See What I See? Art Show | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art painting at the Body Love Ball",
+        image: "./assets/events/BodyLoveBallLivePainting.jpg",
+        imageAlt: "Frizz Kid Art painting at the Body Love Ball | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art painting at the Nikki Minaj Live Painting",
+        image: "./assets/events/NikkiMinajLivePainting.jpg",
+        imageAlt: "Frizz Kid Art painting at the Nikki Minaj Live Painting | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art painting at the Grace Jones Live Painting",
+        image: "./assets/events/GraceJonesLivePainting-2.jpg",
+        imageAlt: "Frizz Kid Art painting at the Grace Jones Live Painting | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art painting at the Grace Jones Live Painting",
+        image: "./assets/events/GraceJonesLivePainting.jpg",
+        imageAlt: "Frizz Kid Art painting at the Grace Jones Live Painting | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi at an Amy Winehouse Live Painting Event",
+        image: "./assets/events/AmyWinehousePainting2017-4.jpg",
+        imageAlt: "Hana Shafi at an Amy Winehouse Live Painting Event | Frizz Kid Art"
+    },
+    {
+        title: "Amy Winehouse Live Painting Event Finished Piece",
+        image: "./assets/events/AmyWinehousePainting2017-3.jpg",
+        imageAlt: "Amy Winehouse Live Painting Event Finished Piece | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi Playing Look-a-like with Amy Winehouse",
+        image: "./assets/events/AmyWinehousePainting2017-2.jpg",
+        imageAlt: "Hana Shafi Playing Look-a-like with Amy Winehouse | Frizz Kid Art"
+    },
+    {
+        title: "Hana Shafi at an Amy Winehouse Live Painting Event",
+        image: "./assets/events/AmyWinehousePainting2017.jpg",
+        imageAlt: "Hana Shafi at an Amy Winehouse Live Painting Event | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art at Fan Expo 2017",
+        image: "./assets/events/FanExpo2017-2.jpg",
+        imageAlt: "Frizz Kid Art at Fan Expo 2017 | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art at Fan Expo 2017",
+        image: "./assets/events/FanExpo2017.jpg",
+        imageAlt: "Frizz Kid Art at Fan Expo 2017 | Frizz Kid Art"
+    },
+    {
+        title: "Women Who Inspire Award 2017",
+        image: "./assets/events/WomenWhoInspireAward2017.jpg",
+        imageAlt: "Women Who Inspire Award 2017 | Frizz Kid Art"
     }
 ]
 
@@ -426,6 +611,31 @@ const commissionArray = [
         title: "Second Poster Frizz Kid Art created for the Toronto Rape Crisis Center",
         image: "./assets/events/SecondPosterforTRCConTheSubway.jpg",
         imageAlt: "Second Poster Frizz Kid Art created for the Toronto Rape Crisis Center | Frizz Kid Art"
+    },
+    {
+        title: "First Poster Frizz Kid Art created for the Toronto Rape Crisis Center",
+        image: "./assets/events/FirstPosterforTRCConTheSubway.jpg",
+        imageAlt: "First Poster Frizz Kid Art created for the Toronto Rape Crisis Center | Frizz Kid Art"
+    },
+    {
+        title: "Program for AGO 2019 by Frizz Kid Art",
+        image: "./assets/events/ProgramForAGO2019.jpg",
+        imageAlt: "Program for AGO 2019 by Frizz Kid Art | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art Dare to Be Aware Campaign Art",
+        image: "./assets/events/DareToBeAware-3.jpg",
+        imageAlt: "Frizz Kid Art Dare to Be Aware Campaign Art | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art Dare to Be Aware Campaign Art",
+        image: "./assets/events/DareToBeAware-2.jpg",
+        imageAlt: "Frizz Kid Art Dare to Be Aware Campaign Art | Frizz Kid Art"
+    },
+    {
+        title: "Frizz Kid Art Dare to Be Aware Campaign Art",
+        image: "./assets/events/DareToBeAware.jpg",
+        imageAlt: "Frizz Kid Art Dare to Be Aware Campaign Art | Frizz Kid Art"
     }
 ]
 
